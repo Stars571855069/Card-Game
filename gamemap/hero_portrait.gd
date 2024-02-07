41929508e3bafd2b235d9950e4f519422750ad3f
Stars <stars571855069@gmail.com>
2024-02-07 16:35:13 +0800

    updated
    1.a played card will have +1 attack when initially have 0 attack and was played by enemy
    2.add damage number display and heal display to all cards and player portrait
    3.poisionous and divine shield now have visual effect when take on effect
    3.card in hand are now aligned left

--- gamemap/hero_portrait.gd
+++ gamemap/hero_portrait.gd
@@ -1,12 +1,24 @@
 extends Node2D
+@export var portrait_route:String
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if portrait_route!=null:
+		character_load(portrait_route)
 	
-func get_hurt():
+
+func character_load(route:String):
+	$PanelContainer/Sprite2D.texture=load(route)
+	
+	
+func get_hurt(amount:int):
+	$dmglabel.text="-"+String.num_int64(amount)
 	$AnimationPlayer.play("get_hurt")
+	
+func get_heal(amount:int):
+	$heallabel.text="+"+String.num_int64(amount)
+	$AnimationPlayer.play("get_heal")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
